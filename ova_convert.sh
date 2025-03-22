6bf50c4ed17dcba366266a1b57dff67f75a88be1
Gines Moratalla <ginesmoratalla@gmail.com>
2025-03-22 20:02:46 +0100

    Working script

---
+++ ova_convert.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Check if target file was provided
+if [[ -z "$1" ]];
+  then
+    echo "[x] Target .ova file not provided as input"
+    exit
+  else
+    echo "[✓] Target file passed: $1"
+fi
+
+OVA_FILE="$1"
+
+# Check for root privileges
+if [[ "$EUID" -ne 0 ]];
+  then
+    echo "[x] Script must be run with root privileges"
+    exit
+fi
+
+# Extract ova file
+mkdir ova_extracted
+tar -xvf $OVA_FILE -C ova_extracted
+if [[ $? -eq 0 ]];
+  then
+    echo "[✓] "$OVA_FILE" extracted succesfuly"
+  else
+    echo "[x] Failed to extract "$OVA_FILE""
+    exit
+fi
+
+
+cd ova_extracted
+VMDK_FILE=$(ls . | grep ".vmdk" | xargs -I {} echo {})
+IFS='.' read -ra ADDR <<< "$VMDK_FILE"
+
+# Find the vdmk files
+echo "DEBUG "$VMDK_FILE" "${ADDR[0]}""
+
+qemu-img convert -f vmdk -O qcow2 "$VMDK_FILE" "${ADDR[0]}.qcow2"
+if [[ $? -eq 0 ]];
+  then
+    echo "[✓] QEMU qcow2 image (${ADDR[0]}) created succesfully"
+  else
+    echo "[x] Failed to convert vmdk disk to qcow2"
+    exit
+fi
